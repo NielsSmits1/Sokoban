91e1911e324682152c7f1ed052a7e44ee8cb51e5
PidgeotClaw <49674820+PidgeotClaw@users.noreply.github.com>
2019-09-09 09:35:42 +0200

    Sokoban base spulleke
    Zoutelande

---
+++ Sokoban/Sokoban/Domain/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sokoban.Domain
+{
+    public class Board
+    {
+        private int _amountOfChars = 0;
+        private Spot[] _bord;
+
+        public Board()
+        {
+            _bord = new Spot[_amountOfChars];
+        }
+    }
+}
---
+++ Sokoban/Sokoban/Domain/Destination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sokoban.Domain
+{
+    public class Destination : Floor
+    {
+
+        public Destination()
+        {
+            _symbol = 'x';
+            _magBezetWorden = true;
+        }
+
+        override public void move()
+        {
+
+        }
+    }
+}
